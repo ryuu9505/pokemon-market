53fb816c1995e19960620079def0c76bbe210fcb
ryuu <ryuu.public@gmail.com>
2022-03-25 08:33:37 +0900

    FEAT item detail

--- src/main/java/market/pokemon/web/PokemonController.java
+++ src/main/java/market/pokemon/web/PokemonController.java
@@ -1,13 +1,25 @@
 package market.pokemon.web;
 
+import market.pokemon.domain.item.Item;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/items")
 public class PokemonController {
 
     @GetMapping("/form")
     public String form() {
         return "form";
     }
+
+/*    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model) {
+
+        return "item";
+    }*/
+
 }
